f1adf78bb8428b7fedf289bf3371fae69b949183
Noah Huppert <contact@noahh.io>
2020-10-26 14:48:19 -0400

    area: made polygon draw on map

--- area.js
+++ area.js
@@ -2,13 +2,20 @@ import 'ol/ol.css';
 import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
-import OSM from 'ol/source/OSM';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Vector as VectorLayer } from 'ol/layer';
+import Feature from 'ol/Feature';
 import { LonLat } from 'ol/proj';
 import Projection from 'ol/proj/Projection';
 import {
     Pointer as PointerInteraction,
     defaults as defaultInteractions
 } from 'ol/interaction';
+import { Polygon } from 'ol/geom';
+
+import MultiPoint from 'ol/geom/MultiPoint';
+import {Fill, Stroke, Style} from 'ol/style';
+
 
 /**
  * The geographical projection used in this project. What us laymen usually
@@ -55,3 +62,71 @@ let map = new Map({
         zoom: 13,
     }),
 });
+
+// Draw sample area
+let vecSrc = new VectorSource({
+    features: [
+	   new Feature({
+		  geometry: new Polygon([
+			 // First "ring" defines border
+			 [
+				[-70.995, 42.005],
+				[-71, 42.013],
+				[-71.007, 42.018],
+				[-71.014, 42.018],
+				[-71.021, 42.016],
+				[-71.023, 42.009],
+				[-71.034, 42.005],
+				[-71.029, 41.995],
+				[-71.023, 41.99],
+				[-70.997, 42.001],
+				[-70.995, 42.005],
+			 ]
+		  ]),
+	   }),
+    ],
+});
+let vecLay = new VectorLayer({
+    source: vecSrc,
+    style: new Style({
+	   fill: new Fill({
+		  color: 'rgba(175, 81, 245, 0.5)',
+	   }),
+	   stroke: new Stroke({
+		  color: '#777777',
+		  width: 1,
+	   }),
+    }),
+});
+map.addLayer(vecLay);
+
+/*
+let source = new VectorSource({
+    features: [
+	   new Feature({
+		  geometry: new Polygon([
+			 [
+				[-5e6, 6e6],
+				[-5e6, 8e6],
+				[-3e6, 8e6],
+				[-3e6, 6e6],
+				[-5e6, 6e6]
+			 ]
+		  ]),
+	   }),
+    ],
+});
+
+var layer = new VectorLayer({
+  source: source,
+});
+
+let map = new Map({
+  layers: [layer],
+  target: 'mapbox',
+  view: new View({
+    center: [0, 3000000],
+    zoom: 2,
+  }),
+});
+*/
