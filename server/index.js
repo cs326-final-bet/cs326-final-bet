1a380830259bfd533e9c0ef63a7b6adaa2a0cad7
Noah Huppert <contact@noahh.io>
2020-11-04 01:23:19 -0500

    mocked get areas endpoint (closes #32)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,46 @@
 import express from 'express';
 
+/**
+ * From: https://stackoverflow.com/a/1527820
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Break any area up into 0.01 mile square boxes.
+ */
+function polysForExt(extent) {
+    const polys = [];
+
+    function r(v) {
+        return Math.round((v + Number.EPSILON) * 100) / 100;
+    }
+    
+    const extBegin = [ extent[0], extent[1] ].map(r).map(v => v - 0.01);
+    const extEnd = [ extent[2], extent[3] ].map(r).map(v => v + 0.01);
+
+    for (let x = extBegin[0]; x < extEnd[0]; x += 0.01) {
+        for (let y = extBegin[1]; y < extEnd[1]; y += 0.01) {
+            polys.push([
+                [x, y],
+                [x, y + 0.01],
+                [x + 0.01, y + 0.01],
+                [x + 0.01, y],
+            ]);
+        }
+    }
+
+    return polys;
+}
+
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -9,7 +50,69 @@ app.get('/', (req, res) => {
     res.redirect('/area.html');
 });
 
+app.get(`/areas`, (req, res) => {
+    // Check extent parameter
+    let extStr = req.query.extent;
+
+    if (extStr === undefined) {
+	   return res
+		  .status(400)
+		  .send({
+			 error: '\"extent\" URL query parameter required'
+		  });
+    }
+    
+    let extParts = extStr.split(',');
+    let extBad = false;
+    
+    if (extParts.length !== 4) {
+	   extBad = true;
+    }
+
+    const extent = extParts.map(s => parseInt(s));
+    if (extent.filter(i => isNaN(i)).length > 0) {
+	   extBad = true;
+    }
+
+    if (extBad === true) {
+	   return res
+		  .status(400)
+		  .send({
+			 error: '\"extent\" URL query parameter must be in the format: <top left latitude>,<top left longitude>,<bottom right latitude>,<bottom right longitude>',
+		  });
+    }
+
+    // Generate fake extent
+    const polys = polysForExt(extent);
+    const areas = polys.map((poly) => {
+	   let trackIds = [];
+	   const numTrackIds = getRandomInt(1, 10);
+	   for (let i = 0; i < numTrackIds; i += 1) {
+		  let n = getRandomInt(0, 1000);
+
+		  if (trackIds.indexOf(n) === -1) {
+			 trackIds.push(n);
+		  }
+	   }
+	   
+	   return {
+		  position: {
+			 latitude: poly[0],
+			 longitude: poly[1],
+		  },
+		  trackIds: trackIds,
+		  ownerId: getRandomInt(0, 1000),
+	   };
+    });
+
+    return res.send({
+	   areas: areas,
+    });
+});
+
 app.listen(port, () => {
-    console.log(`Server listening on :${port}`);
-    console.log(`You should be able to view the server in your web browser at http://127.0.0.1:${port} or http://localhost:${port}`);
+    console.log(`\
+Server listening on port ${port}. View in your web browser:
+
+    http://127.0.0.1:${port} or http://localhost:${port}`);
 });
